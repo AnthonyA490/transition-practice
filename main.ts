0fca3f6d1d2746f0fa1dec379a9dba6408a3a473
AnthonyA490 <113041244+AnthonyA490@users.noreply.github.com>
2022-09-20 10:37:59 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,51 @@
 input.onButtonPressed(Button.A, function () {
     radio.sendString(Hello)
 })
-radio.onReceivedString(function (receivedString) {
-    basic.showString(receivedString)
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
 })
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == Hello) {
+        basic.showLeds(`
+            # . . . .
+            # # . . .
+            # # # . .
+            # # # # .
+            # # # # #
+            `)
+    }
+    if (receivedString == animate) {
+        for (let index = 0; index < 3; index++) {
+            basic.showLeds(`
+                # . . . .
+                # # . . .
+                # # # . .
+                # # # # .
+                # # # # #
+                `)
+            basic.pause(100)
+            basic.showLeds(`
+                . . . . #
+                . . . # #
+                . . # # #
+                . # # # #
+                # # # # #
+                `)
+            basic.pause(100)
+        }
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendString(animate)
+})
+let animate = ""
 let Hello = ""
 Hello = "Hello"
+animate = "animate"
 radio.setGroup(77)
